c6bdfa65cf528c57073acb39817f4db45add4011
Supriya Mahato <codewithapu@gmail.com>
2023-12-20 19:19:16 +0530

    added cards in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,32 +60,96 @@ const Home: React.FC = () => {
 
                 <div className={styles.Originals}>
                     <div className={styles.OriginalCards}>
-                        <h1>
-                            new release
-                        </h1>
-                        <div className={styles.Card}>
-                            <Link draggable="false" className={styles.CardLink} href="/">
-                                <a>
-                                    <img className={styles.CardBanner} src="https://ik.imagekit.io/eduburner/DesignScape/bannercarddesignscape.png?updatedAt=1702930459000" draggable="false" />
-
-                                </a>
-                            </Link>
-                            <button className={styles.CardButton}>
-                                odyssey launching on
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="34"
-                                    height="34"
-                                    viewBox="0 0 54 54"
-                                    fill="none"
-                                >
-                                    <path
-                                        d="M42.4794 16.6155V19.4483C42.4794 20.7813 42.7849 21.92 43.3958 22.8642C44.0346 23.8362 44.9371 24.4889 46.1036 24.8221L2 24.8221V28.1547L46.1036 28.1547C44.9371 28.488 44.0346 29.1268 43.3958 30.071C42.7849 31.043 42.4794 32.1956 42.4794 33.5286V36.3613L46.0619 36.3613V33.9035C46.0619 32.1817 46.5202 30.7792 47.4366 29.6961C48.3531 28.613 49.6306 28.0714 51.2691 28.0714H51.769V24.9054H51.2691C49.6306 24.9054 48.3531 24.3639 47.4366 23.2808C46.5202 22.1977 46.0619 20.7952 46.0619 19.0733V16.6155H42.4794Z"
-                                        fill="#111"
-                                    />
+                        <div className={styles.SectionTitle}>
+                            <h1 className={styles.Title}>burner academy</h1>
+                            <h3 className={styles.Description}>
+                                <svg className={styles.Bullet} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" fill="none">
+                                    <circle cx="50" cy="50" r="50" />
                                 </svg>
-                            </button>
+                                Refined, exclusive courses <br />
+                                reimagined experience</h3>
                         </div>
+
+                        <div className={styles.CardsBlock}>
+                            <div className={styles.Card}>
+                                <div className={styles.CardBanner}>
+                                    <h3 className={styles.Badge}>trending</h3>
+                                    <img
+                                        className={styles.CardBannerImage}
+                                        src="https://ik.imagekit.io/eduburner/Essentials/Edu%20Burner%20Pro?updatedAt=1702292814779https://ik.imagekit.io/eduburner/Essentials/Edu%20Burner%20Pro?updatedAt=1702292814779"
+                                        draggable="false"
+                                    />
+                                </div>
+
+                                <div className={styles.CardDetails}>
+                                    <h1 className={styles.CardTitle}>Design Scape : Odyssey to human centric design crafting user interfaces</h1>
+                                    <p className={styles.CardDescription}>
+                                        Learn Figma, variables, building scalable design systems, pages with latest design trends.
+                                    </p>
+                                </div>
+
+                                <div className={styles.Pricing}>
+                                    <img draggable="false" className={styles.AuthorBadge} src="/dtficon.png" />
+                                    <p className={styles.Price}>₹ 3499</p>
+                                </div>
+                                <button className={styles.CardButton}>
+                                    grab your seat now
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="34"
+                                        height="34"
+                                        viewBox="0 0 54 54"
+                                        fill="none"
+                                    >
+                                        <path
+                                            d="M42.4794 16.6155V19.4483C42.4794 20.7813 42.7849 21.92 43.3958 22.8642C44.0346 23.8362 44.9371 24.4889 46.1036 24.8221L2 24.8221V28.1547L46.1036 28.1547C44.9371 28.488 44.0346 29.1268 43.3958 30.071C42.7849 31.043 42.4794 32.1956 42.4794 33.5286V36.3613L46.0619 36.3613V33.9035C46.0619 32.1817 46.5202 30.7792 47.4366 29.6961C48.3531 28.613 49.6306 28.0714 51.2691 28.0714H51.769V24.9054H51.2691C49.6306 24.9054 48.3531 24.3639 47.4366 23.2808C46.5202 22.1977 46.0619 20.7952 46.0619 19.0733V16.6155H42.4794Z"
+                                            fill="#111"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
+
+                            <div className={styles.Card}>
+                                <div className={styles.CardBanner}>
+                                    <h3 className={styles.Badge}>trending</h3>
+                                    <img
+                                        className={styles.CardBannerImage}
+                                        src="https://ik.imagekit.io/eduburner/Essentials/Edu%20Burner%20Pro?updatedAt=1702292814779https://ik.imagekit.io/eduburner/Essentials/Edu%20Burner%20Pro?updatedAt=1702292814779"
+                                        draggable="false"
+                                    />
+                                </div>
+
+                                <div className={styles.CardDetails}>
+                                    <h1 className={styles.CardTitle}>Design Scape : Odyssey to human centric design crafting user interfaces</h1>
+                                    <p className={styles.CardDescription}>
+                                        Learn Figma, variables, components, building scalable design systems, pages with latest design trends.
+                                    </p>
+                                </div>
+
+                                <div className={styles.Pricing}>
+                                    <img draggable="false" className={styles.AuthorBadge} src="/dtficon.png" />
+                                    <p className={styles.Price}>₹ 3499</p>
+                                </div>
+                                <button className={styles.CardButton}>
+                                    grab your seat now
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="34"
+                                        height="34"
+                                        viewBox="0 0 54 54"
+                                        fill="none"
+                                    >
+                                        <path
+                                            d="M42.4794 16.6155V19.4483C42.4794 20.7813 42.7849 21.92 43.3958 22.8642C44.0346 23.8362 44.9371 24.4889 46.1036 24.8221L2 24.8221V28.1547L46.1036 28.1547C44.9371 28.488 44.0346 29.1268 43.3958 30.071C42.7849 31.043 42.4794 32.1956 42.4794 33.5286V36.3613L46.0619 36.3613V33.9035C46.0619 32.1817 46.5202 30.7792 47.4366 29.6961C48.3531 28.613 49.6306 28.0714 51.2691 28.0714H51.769V24.9054H51.2691C49.6306 24.9054 48.3531 24.3639 47.4366 23.2808C46.5202 22.1977 46.0619 20.7952 46.0619 19.0733V16.6155H42.4794Z"
+                                            fill="#111"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+
+
+
                     </div>
                 </div>
 
